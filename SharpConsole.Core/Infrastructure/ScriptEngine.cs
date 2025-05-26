3f17b05baa02ac875508c07b98ca490a0ccf25f6
o_porpino <dev@oporpino.com>
2025-05-26 15:35:55 -0300

    fix: fix evaluation of var in console

--- SharpConsole.Core/Infrastructure/ScriptEngine.cs
+++ SharpConsole.Core/Infrastructure/ScriptEngine.cs
@@ -7,26 +7,49 @@ using SharpConsole.Core.Application;
 using System.Collections.Generic;
 using System.Dynamic;
 using System.Runtime.CompilerServices;
+using System.Reflection;
 
 namespace SharpConsole.Core.Infrastructure;
 
 public class ScriptEngine : IScriptEngine
 {
+  private ScriptState<object>? _scriptState;
+
   public async Task<object?> Execute(string command)
   {
     var context = ConsoleContext.Get();
     try
     {
+      var assemblies = AppDomain.CurrentDomain.GetAssemblies()
+        .Where(a => !a.IsDynamic && !string.IsNullOrEmpty(a.Location))
+        .ToList();
+
+      var exampleAssemblies = assemblies
+        .Where(a => a.GetName().Name?.StartsWith("SharpConsoleExamples") == true)
+        .ToList();
+
       var options = ScriptOptions.Default
         .WithImports("System", "System.Linq", "System.Collections.Generic", "System.Dynamic", "System.Runtime.CompilerServices")
-        .AddReferences(typeof(Enumerable).Assembly)
-        .AddReferences(typeof(Dictionary<string, object>).Assembly)
-        .AddReferences(typeof(ExpandoObject).Assembly)
-        .AddReferences(typeof(DynamicAttribute).Assembly)
+        .AddReferences(assemblies)
         .WithEmitDebugInformation(true);
 
-      var scriptState = await CSharpScript.RunAsync(command, options, context);
-      return scriptState.ReturnValue;
+      if (exampleAssemblies.Any())
+      {
+        var exampleNamespaces = exampleAssemblies
+          .SelectMany(a => a.GetTypes())
+          .Select(t => t.Namespace)
+          .Where(n => n != null)
+          .Distinct()
+          .ToList();
+
+        options = options.WithImports(exampleNamespaces);
+      }
+
+      _scriptState = _scriptState == null
+        ? await CSharpScript.RunAsync(command, options, context)
+        : await _scriptState.ContinueWithAsync(command);
+
+      return _scriptState.ReturnValue;
     }
     catch (Exception ex)
     {
